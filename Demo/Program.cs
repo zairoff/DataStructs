e3198a76211e02f13d87ad0402b4e193f43d8f1d
zairoff <marufzoiroff@gmail.com>
2021-11-26 15:04:25 +0500

    added demo console app to test features

---
+++ Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Task1;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new LinkedList<string>();
+
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            list.Add("4");
+
+            list.AddAt(2, "A");
+
+            list.Print();
+        }
+    }
+}
