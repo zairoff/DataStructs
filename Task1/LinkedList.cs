9a16cd20b7332cd0ce530b40d36b8b62c732590e
zairoff <marufzoiroff@gmail.com>
2021-11-26 16:49:04 +0500

    implemented RemoveAt feature

--- Task1/LinkedList.cs
+++ Task1/LinkedList.cs
@@ -47,7 +47,7 @@ namespace Task1
         public void Print()
         {
             var runner = _first;
-            while(runner != null)
+            while (runner != null)
             {
                 Console.WriteLine(runner._data);
                 runner = runner._next;
@@ -56,7 +56,7 @@ namespace Task1
 
         public void AddAt(int index, T item)
         {
-            if (index > _count)
+            if (index > _count || index < 0)
                 throw new IndexOutOfRangeException();
 
             var node = new Node<T>(item);
@@ -69,7 +69,7 @@ namespace Task1
             }
 
             // check if insertion to the beginning
-            if(index == 0)
+            if (index == 0)
             {
                 node._next = _first;
                 _first = node;
@@ -78,7 +78,7 @@ namespace Task1
             }
 
             // check if insertion to the end
-            if(index == _count)
+            if (index == _count)
             {
                 _last._next = node;
                 _last = node;
@@ -95,12 +95,13 @@ namespace Task1
             _count++;
         }
 
-        public void Remove(T item) 
+        public void Remove(T item)
         {
             var previous = _first;
             var runner = _first;
 
-            while(runner != null)
+            int index = 0;
+            while (runner != null)
             {
                 if (item.CompareTo(runner._data) == 0)
                 {
@@ -110,7 +111,40 @@ namespace Task1
 
                 previous = runner;
                 runner = runner._next;
-            }            
+                index++;
+            }
+
+            if (index == 0)
+                _first = previous._next;
+
+            if (index == (_count - 1))
+                _last = previous;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index >= _count || index < 0)
+                throw new IndexOutOfRangeException();
+
+            if(index == 0)
+            {
+                _first = _first._next;
+                return;
+            }
+
+            var previous = _first;
+            var runner = _first;
+            var cnt = index;
+            while(cnt-- > 0)
+            {
+                previous = runner;
+                runner = runner._next;
+            }
+
+            previous._next = runner._next;
+
+            if (index == (_count - 1))
+                _last = previous;
         }
     }
 }
