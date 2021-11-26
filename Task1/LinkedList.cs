dc6f4ef02eefa23c9d14a0340bae715484947a01
zairoff <marufzoiroff@gmail.com>
2021-11-26 12:21:28 +0500

    implemented Add feature

--- Task1/LinkedList.cs
+++ Task1/LinkedList.cs
@@ -2,7 +2,35 @@
 
 namespace Task1
 {
-    public class Class1
+    public class LinkedList<T>
     {
+        private class Node<T>
+        {
+            public T _data;
+            public Node<T> _next;
+
+            public Node(T data)
+            {
+                _data = data;
+            }
+        }
+
+        private Node<T> _first;
+        private Node<T> _last;
+
+        public void Add(T item)
+        {
+            var node = new Node<T>(item);
+
+            if (_first == null)
+                _first = _last = node;
+            else
+            {
+                _last._next = node;
+                _last = node;
+            }
+
+        }
+
     }
 }
